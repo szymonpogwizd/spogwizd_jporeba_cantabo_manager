4bd573aecaf77d121256e51e5302dcaf62b559fb
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-04-03 21:14:22 +0200

    Create DAO and DTO class for Profile (#17)

---
+++ src/main/java/pl/cantabo/database/profile/ProfileDAO.java
@@ -0,0 +1,59 @@
+package pl.cantabo.database.profile;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.UUID;
+
+@Entity
+@Builder
+@EqualsAndHashCode(callSuper = false)
+@Data
+@Table(name = "profiles")
+public class ProfileDAO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(columnDefinition = "text")
+    @NotEmpty
+    private String name;
+
+    private Boolean active;
+
+    private boolean sortByUsed;
+
+    private int bgColor;
+
+    private int txColor;
+
+    private int stopColor;
+
+    private String fontStyle;
+
+    private float margin;
+
+    private float maxFont;
+
+    private float maxMin;
+
+    private String align;
+
+    private String algorithmRange;
+
+    private boolean showTitle;
+
+    private boolean allBig;
+
+    private boolean showEmptySlide;
+
+    private boolean invertColors;
+
+    private boolean expandedList;
+
+    private boolean defaultItem;
+}
---
+++ src/main/java/pl/cantabo/database/profile/ProfileInfoDTO.java
@@ -0,0 +1,41 @@
+package pl.cantabo.database.profile;
+
+import lombok.Data;
+
+@Data
+public class ProfileInfoDTO {
+
+    private String name;
+
+    private boolean active;
+
+    private boolean sortByUsed;
+
+    private int bgColor;
+
+    private int txColor;
+
+    private int stopColor;
+
+    private String fontStyle;
+
+    private float margin;
+
+    private float maxFont;
+
+    private float maxMin;
+
+    private String align;
+
+    private String algorithmRange;
+
+    private boolean showTitle;
+
+    private boolean allBig;
+
+    private boolean showEmptySlide;
+
+    private boolean invertColors;
+
+    private boolean expandedList;
+}
