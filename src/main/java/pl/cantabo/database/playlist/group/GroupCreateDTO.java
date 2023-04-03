64e5af7b08137432a885b9ab3525277bc0cdff69
Jporebaa <jakubporeba@gmail.com>
2023-04-03 21:56:34 +0200

    Creatin DAO and DTO class for Playlists.

---
+++ src/main/java/pl/cantabo/database/playlist/group/GroupCreateDTO.java
@@ -0,0 +1,13 @@
+package pl.cantabo.database.playlist.group;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class GroupCreateDTO {
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String name;
+}
