599553db1058bb560a6e5678204473ef87cceacb
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-04-03 19:53:41 +0200

    Package change

--- src/main/java/pl/cantabo/database/user/UserCreateDTO.java
+++ src/main/java/pl/cantabo/database/user/UserCreateDTO.java
@@ -1,4 +1,4 @@
-package pl.cantabo.database;
+package pl.cantabo.database.user;
 
 import lombok.Data;
 
--- src/main/java/pl/cantabo/database/user/UserDAO.java
+++ src/main/java/pl/cantabo/database/user/UserDAO.java
@@ -1,4 +1,4 @@
-package pl.cantabo.database;
+package pl.cantabo.database.user;
 
 import jakarta.persistence.*;
 import lombok.Builder;
