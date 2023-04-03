8c1f8e7aa138de340eb3d9718e78a522ab61c1f1
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-04-03 15:34:57 +0200

    Initial project

---
+++ src/main/java/pl/cantabo/CantaboApplication.java
@@ -0,0 +1,13 @@
+package pl.cantabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/cantabo/CantaboApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cantabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
