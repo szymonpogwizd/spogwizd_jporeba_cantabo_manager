ffbe5058c0f8a12c49e1b99145a19ea7145ebd3c
Jakub Poręba <114797801+Jporebaa@users.noreply.github.com>
2023-04-03 21:39:22 +0200

    Creatin DAO and DTO class for Playlists. (#19)
    Co-authored-by: Jporebaa <jakubporeba@gmail.com>

---
+++ src/main/java/pl/cantabo/database/playlist/PlaylistCreateDTO.java
@@ -0,0 +1,13 @@
+package pl.cantabo.database.playlist;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class PlaylistCreateDTO {
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String name;
+}
