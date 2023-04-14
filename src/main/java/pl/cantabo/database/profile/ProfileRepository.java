803ca75a83fa51dcbc98091b8caaa856add4bcf9
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-04-14 18:23:36 +0200

    Create ProfileRepository (#55)

---
+++ src/main/java/pl/cantabo/database/profile/ProfileRepository.java
@@ -0,0 +1,15 @@
+package pl.cantabo.database.profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<ProfileDAO, UUID> {
+
+    @Query("SELECT p FROM ProfileDAO p WHERE p.name LIKE %:nameFragment%")
+    List<ProfileDAO> findProfileByName(String nameFragment);
+}
--- src/test/java/pl/cantabo/database/user/UserRepositoryTest.java
+++ src/test/java/pl/cantabo/database/user/UserRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import pl.cantabo.database.song.songCategory.SongCategoryDAO;
 import pl.cantabo.database.user.factory.UserDAOFactory;
 
 import java.util.List;
@@ -74,14 +73,36 @@ class UserRepositoryTest {
         // given
         UserDAO userDAO = UserDAOFactory.defaultBuilder().build();
         UserDAO userDAO2 = UserDAOFactory.defaultBuilder().build();
-
-        // when
         userRepository.saveAndFlush(userDAO);
         userRepository.saveAndFlush(userDAO2);
+
+        // when
         List<UserDAO> userDAOList = userRepository.findAll();
 
         // then
         assertEquals(2, userDAOList.size());
+    }
+
+    @Test
+    public void findUserByUserNameOrEmailTest() {
+        // given
+        UserDAO user1 = UserDAOFactory.defaultBuilder().name("user1").email("user1@example.com").build();
+        UserDAO user2 = UserDAOFactory.defaultBuilder().name("user2").email("user2@example.com").build();
+        UserDAO user3 = UserDAOFactory.defaultBuilder().name("user3").email("user3@example.pl").build();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+
+        // when
+        List<UserDAO> userUser1 = userRepository.findUserByUserNameOrEmail("user1");
+        List<UserDAO> userExam = userRepository.findUserByUserNameOrEmail("exam");
+        List<UserDAO> userCom = userRepository.findUserByUserNameOrEmail("com");
+
+        // then
+        assertEquals(1, userUser1.size());
+        assertEquals(3, userExam.size());
+        assertEquals(2, userCom.size());
     }
 
     @Test
@@ -105,27 +126,5 @@ class UserRepositoryTest {
         assertTrue(userTypeUser.contains(user1));
         assertTrue(userTypeUser.contains(user2));
         assertTrue(userTypeAdministrator.contains(admin));
-    }
-
-    @Test
-    public void findUserByUserNameOrEmailTest() {
-        // given
-        UserDAO user1 = UserDAOFactory.defaultBuilder().name("user1").email("user1@example.com").build();
-        UserDAO user2 = UserDAOFactory.defaultBuilder().name("user2").email("user2@example.com").build();
-        UserDAO user3 = UserDAOFactory.defaultBuilder().name("user3").email("user3@example.pl").build();
-
-        userRepository.save(user1);
-        userRepository.save(user2);
-        userRepository.save(user3);
-
-        // when
-        List<UserDAO> userUser1 = userRepository.findUserByUserNameOrEmail("user1");
-        List<UserDAO> userExam = userRepository.findUserByUserNameOrEmail("exam");
-        List<UserDAO> userCom = userRepository.findUserByUserNameOrEmail("com");
-
-        // then
-        assertEquals(1, userUser1.size());
-        assertEquals(3, userExam.size());
-        assertEquals(2, userCom.size());
     }
 }
