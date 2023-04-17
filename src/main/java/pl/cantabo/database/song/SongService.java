afcfcaa508119e355ee193e9a1ab52dec3364236
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-04-17 23:14:45 +0200

    Create SongService

---
+++ src/main/java/pl/cantabo/database/song/SongService.java
@@ -0,0 +1,31 @@
+package pl.cantabo.database.song;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class SongService {
+
+    private final SongRepository songRepository;
+
+    public SongDAO create(SongDAO song) {
+        log.debug("Creating song {}", song);
+        return log.traceExit(songRepository.save(song));
+    }
+
+    public void delete(UUID id) {
+        log.debug("Deleting song {}", id);
+        songRepository.deleteById(id);
+    }
+
+    public List<SongDAO> getAll() {
+        log.debug("Getting all songs");
+        return log.traceExit(songRepository.findAll());
+    }
+}
