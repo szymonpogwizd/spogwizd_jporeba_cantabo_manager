9d9cf58f5ec69c4280c77006e9685fed93a743ba
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-04-17 23:22:07 +0200

    Create SongCategoryService (#69)

---
+++ src/main/java/pl/cantabo/database/song/songCategory/SongCategoryService.java
@@ -0,0 +1,31 @@
+package pl.cantabo.database.song.songCategory;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class SongCategoryService {
+
+    private final SongCategoryRepository songCategoryRepository;
+
+    public SongCategoryDAO create(SongCategoryDAO songCategory) {
+        log.debug("Creating song category {}", songCategory);
+        return log.traceExit(songCategoryRepository.save(songCategory));
+    }
+
+    public void delete(UUID id) {
+        log.debug("Deleting song category {}", id);
+        songCategoryRepository.deleteById(id);
+    }
+
+    public List<SongCategoryDAO> getAll() {
+        log.debug("Getting all song categories");
+        return log.traceExit(songCategoryRepository.findAll());
+    }
+}
---
+++ src/test/java/pl/cantabo/database/song/songCategory/SongCategoryServiceTest.java
@@ -0,0 +1,69 @@
+package pl.cantabo.database.song.songCategory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.cantabo.database.song.songCategory.factory.SongCategoryDAOFactory;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class SongCategoryServiceTest {
+
+    private SongCategoryService songCategoryService;
+
+    private SongCategoryRepository songCategoryRepository;
+
+    @BeforeEach
+    public void init() {
+        songCategoryRepository = Mockito.mock(SongCategoryRepository.class);
+        songCategoryService = new SongCategoryService(songCategoryRepository);
+    }
+
+    @Test
+    void create() {
+        // given
+        SongCategoryDAO songCategory = SongCategoryDAOFactory.defaultBuilder().build();
+
+        // when
+        songCategoryService.create(songCategory);
+
+        // then
+        verify(songCategoryRepository, times(1)).save(songCategory);
+        assertNotNull(songCategory);
+    }
+
+    @Test
+    void delete() {
+        // given
+        UUID id = UUID.randomUUID();
+
+        // when
+        songCategoryService.delete(id);
+
+        // then
+        verify(songCategoryRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    void getAll() {
+        // given
+        List<SongCategoryDAO> songCategoryList = List.of(
+                SongCategoryDAOFactory.defaultBuilder().build(),
+                SongCategoryDAOFactory.defaultBuilder().build(),
+                SongCategoryDAOFactory.defaultBuilder().build()
+        );
+
+        when(songCategoryRepository.findAll()).thenReturn(songCategoryList);
+
+        // when
+        List<SongCategoryDAO> result = songCategoryService.getAll();
+
+        // then
+        verify(songCategoryRepository, times(1)).findAll();
+        assertEquals(songCategoryList, result);
+    }
+}
