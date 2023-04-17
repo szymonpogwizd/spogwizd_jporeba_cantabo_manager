050336a572ae7e1300cad8931b3e299886e81849
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-04-17 23:15:23 +0200

    Create SongService (#68)

---
+++ src/test/java/pl/cantabo/database/song/SongServiceTest.java
@@ -0,0 +1,70 @@
+package pl.cantabo.database.song;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.cantabo.database.song.factory.SongDAOFactory;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class SongServiceTest {
+
+    private SongService songService;
+
+    private SongRepository songRepository;
+
+    @BeforeEach
+    public void init() {
+        songRepository = Mockito.mock(SongRepository.class);
+        songService = new SongService(songRepository);
+    }
+
+    @Test
+    void create() {
+        // given
+        SongDAO song = SongDAOFactory.defaultBuilder().build();
+
+        // when
+        songService.create(song);
+
+        // then
+        verify(songRepository, times(1)).save(song);
+        assertNotNull(song);
+    }
+
+    @Test
+    void delete() {
+        // given
+        UUID id = UUID.randomUUID();
+
+        // when
+        songService.delete(id);
+
+        // then
+        verify(songRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    void getAll() {
+        // given
+        List<SongDAO> songList = List.of(
+                SongDAOFactory.defaultBuilder().build(),
+                SongDAOFactory.defaultBuilder().build(),
+                SongDAOFactory.defaultBuilder().build()
+        );
+
+        when(songRepository.findAll()).thenReturn(songList);
+
+        // when
+        List<SongDAO> songDAOList = songService.getAll();
+
+        // then
+        verify(songRepository, times(1)).findAll();
+        assertEquals(3, songDAOList.size());
+    }
+}
