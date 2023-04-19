dfe6f3aa3062dfe56baca8361d02887f7a752e36
Jakub Poręba <114797801+Jporebaa@users.noreply.github.com>
2023-04-19 15:31:03 +0200

    Create PlaylistMapper and PlaylistMapperTest (#80)
    Co-authored-by: Jporebaa <jakubporeba@gmail.com>

---
+++ src/test/java/pl/cantabo/database/playlist/PlaylistMapperTest.java
@@ -0,0 +1,60 @@
+package pl.cantabo.database.playlist;
+
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pl.cantabo.database.configuration.MapperConfiguration;
+import pl.cantabo.database.playlist.factory.PlaylistDAOFactory;
+import pl.cantabo.database.playlist.factory.PlaylistDTOFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = MapperConfiguration.class)
+public class PlaylistMapperTest {
+
+    @Autowired
+    private PlaylistMapper playlistMapper;
+
+    @Test
+    void playlistDAO2PlaylistInfoDTO(){
+        //Given
+        PlaylistDAO playlistDAO = PlaylistDAOFactory.defaultBuilder().build();
+        // when
+        PlaylistInfoDTO playlistInfoDTO = playlistMapper.playlistDAO2PlaylistInfoDTO(playlistDAO);
+        //then
+        assertNotNull(playlistInfoDTO);
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(playlistDAO.getName()).isEqualTo(playlistInfoDTO.getName());
+        softly.assertAll();
+    }
+    @Test
+    void playlistCreateDTO2PlaylistDAO(){
+        //given
+        PlaylistCreateDTO playlistCreateDTO = PlaylistDTOFactory.defaultPlaylistCreateDTO();
+
+        //when
+        PlaylistDAO playlistDAO = playlistMapper.playlistCreateDTO2PlaylistDAO();
+        //then
+        assertNotNull(playlistDAO);
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(playlistCreateDTO.getName()).isEqualTo(playlistDAO.getName());
+        softly.assertAll();
+    }
+
+    @Test
+    void PlaylistUpdateDTO2PlaylistDAO(){
+        //given
+        PlaylistUpdateDTO playlistUpdateDTO = PlaylistDTOFactory.defaultPlaylistUpdateDTO();
+        //when
+        PlaylistDAO playlistDAO = playlistMapper.playlistUpdateDTO2PlaylistDAO(playlistUpdateDTO);
+        //then
+        assertNotNull(playlistDAO);
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(playlistUpdateDTO.getName()).isEqualTo(playlistDAO.getName());
+
+    }
+}
---
+++ src/test/java/pl/cantabo/database/playlist/factory/PlaylistDTOFactory.java
@@ -0,0 +1,21 @@
+package pl.cantabo.database.playlist.factory;
+
+import pl.cantabo.database.playlist.PlaylistCreateDTO;
+import pl.cantabo.database.playlist.PlaylistDAO;
+import pl.cantabo.database.playlist.PlaylistUpdateDTO;
+
+public class PlaylistDTOFactory {
+
+    public static PlaylistCreateDTO defaultPlaylistCreateDTO(){
+        PlaylistCreateDTO playlistCreateDTO = new PlaylistCreateDTO();
+        playlistCreateDTO.setName(PlaylistDAOFactory.NAME);
+
+        return playlistCreateDTO;
+    }
+    public static PlaylistUpdateDTO defaultPlaylistUpdateDTO(){
+        PlaylistUpdateDTO playlistUpdateDTO = new PlaylistUpdateDTO();
+        playlistUpdateDTO.setName(PlaylistDAOFactory.NAME);
+        return playlistUpdateDTO;
+    }
+}
+
