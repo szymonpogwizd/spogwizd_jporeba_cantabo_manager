93ad4454d507b2c90ffb614b61d256d965f37951
Jakub Poręba <114797801+Jporebaa@users.noreply.github.com>
2023-05-11 20:21:32 +0200

    Create PlaylistCategoryService and PlaylistCategoryServiceTest (#114)
    Co-authored-by: Jporebaa <jakubporeba@gmail.com>

---
+++ src/test/java/pl/cantabo/service/PlaylistCategoryServiceTest.java
@@ -0,0 +1,69 @@
+package pl.cantabo.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.cantabo.database.playlist.playlistCategory.PlaylistCategoryDAO;
+import pl.cantabo.database.playlist.playlistCategory.PlaylistCategoryRepository;
+import pl.cantabo.database.playlist.playlistCategory.factory.PlaylistCategoryDAOFactory;
+
+import java.util.List;
+import java.util.UUID;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlaylistCategoryServiceTest {
+    private PlaylistCategoryService playlistCategoryService;
+    private PlaylistCategoryRepository playlistCategoryRepository;
+
+    @BeforeEach
+    public void init(){
+        playlistCategoryRepository = Mockito.mock(PlaylistCategoryRepository.class);
+        playlistCategoryService = new PlaylistCategoryService(playlistCategoryRepository);
+    }
+
+
+    @Test
+    void create(){
+        //Given
+        PlaylistCategoryDAO playlistCategory = PlaylistCategoryDAOFactory.defaultBuilder().build();
+
+        //when
+        playlistCategoryService.create(playlistCategory);
+        //then
+        verify(playlistCategoryRepository, times(1)).save(playlistCategory);
+        assertNotNull(playlistCategory);
+    }
+
+    @Test
+    void delete(){
+        //given
+        UUID id = UUID.randomUUID();
+        //when
+        playlistCategoryService.delete(id);
+        //then
+        verify(playlistCategoryRepository, times(1)).deleteById(id);
+
+    }
+
+    @Test
+    void getAll(){
+        //given
+        List<PlaylistCategoryDAO> playlistCategoryList = List.of(
+                PlaylistCategoryDAOFactory.defaultBuilder().build(),
+                PlaylistCategoryDAOFactory.defaultBuilder().build(),
+                PlaylistCategoryDAOFactory.defaultBuilder().build()
+        );
+
+        when(playlistCategoryRepository.findAll()).thenReturn(playlistCategoryList);
+
+        //when
+        List<PlaylistCategoryDAO> result = playlistCategoryService.getAll();
+
+        //then
+
+        verify(playlistCategoryRepository, times(1)).findAll();
+        assertEquals(playlistCategoryList, result);
+    }
+}
