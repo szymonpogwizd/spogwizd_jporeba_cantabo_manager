919a5ff19f511c793c29bb98e50638e3a1934905
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-05-27 12:44:51 +0200

    Add update of slides in song

--- src/main/java/pl/cantabo/controller/SongAndSlideUpdateRequestDTO.java
+++ src/main/java/pl/cantabo/controller/SongAndSlideUpdateRequestDTO.java
@@ -2,14 +2,16 @@ package pl.cantabo.controller;
 
 import lombok.Data;
 import pl.cantabo.database.slide.SlideCreateDTO;
-import pl.cantabo.database.song.SongCreateDTO;
+import pl.cantabo.database.song.SongUpdateDTO;
 
 import javax.validation.Valid;
 import java.util.List;
+
 @Data
-public class SongAndCreateRequestDTO {
+public class SongAndSlideUpdateRequestDTO {
+
     @Valid
-    private SongCreateDTO song;
+    private SongUpdateDTO song;
 
     @Valid
     private List<SlideCreateDTO> slides;
